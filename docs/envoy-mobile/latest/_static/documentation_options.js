75c39cddc57f1852cabf70f554702e1016d21d81
envoy-mobile-docs(ci) <envoy-mobile-docs@users.noreply.github.com>
2023-04-03 16:54:50 +0000

    docs envoy-mobile@cc9bae1db2cf5cfe34e64c7ad94624af6cc8c442

--- docs/envoy-mobile/latest/_static/documentation_options.js
+++ docs/envoy-mobile/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.0-35afb1',
+    VERSION: '0.5.0-cc9bae',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
